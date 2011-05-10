68ca497ff782650a1390034466703126e84c8553
Filip Andersson <andersson.filip@live.se>
2011-05-10 16:06:59 +0200

    Dadam.

--- src/Platform.java
+++ src/Platform.java
@@ -1,4 +1,16 @@
 
-public class Platform {
+import java.awt.Rectangle;
 
+public class Platform extends Entity {
+
+	public Platform(int x, int y) {
+		super(x, y);
+		super.setForm(new Rectangle.Double(x, y, 10, 10));
+	}
+
+	@Override
+	public ImageObject getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
