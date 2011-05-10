dba0113902829fbc1beaad71f687cfe687c41288
gernberg <git@minpunkt.se>
2011-05-10 14:11:01 +0200

    testar

--- src/UserController.java
+++ src/UserController.java
@@ -7,9 +7,14 @@ public class UserController implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+		// Lite kod som ska konflikta.
+		if(arg0.getKeyChar() == 'g'){
+			System.out.println("G är bästa bokstaven!");
+		}else{
+			System.out.println("Du tryckte ner: " + arg0.getKeyChar());
+		}
 	}
-
+	
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
