cc6b05ec807e33e1b547d9fa70419b89ab86bdf8
kangee <markus.fridolfsson@gmail.com>
2011-05-17 10:58:21 +0200

    Lagt till en sammling med alla objekt på planen

--- src/Coordinator.java
+++ src/Coordinator.java
@@ -1,6 +1,16 @@
+import java.util.LinkedList;
+
 /**
  * Denna klass borde hålla koll på allting som finns på banan just nu
  */
 public class Coordinator {
 
+	
+	private LinkedList<Entity> entitys = new LinkedList<Entity>();
+	
+
+	public void addToList(Entity e){
+		entitys.add(e);
+	}
+	
 }
